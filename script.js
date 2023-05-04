b553cbf2aa194baa05e1eda742adefbe955d46e6
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-05-04 10:22:22 -0700

    Adds like button and placeholder video info in HTML + outlines JavaScript to code

---
+++ script.js
@@ -0,0 +1,8 @@
+/* 
+Outline of functions
+1. dynamically populate video + info from db.json on page load
+2. create a randomizer function that returns a random movie object when passed db.json
+3. Connect html id = "randomizer" button to randomizer function
+4. Create another "like" button that saves a video to a sidebar with an id of "saved-videos"
+5. After clicking a video on the side bar, populate main info + video element with video selected
+*/
