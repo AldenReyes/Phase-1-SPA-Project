4727aeed438c173eb6a697ef32ba2a74b311b5e0
Alden Reyes <122179486+AldenReyes@users.noreply.github.com>
2023-05-08 16:09:25 -0700

    adds savedVideos linking savedclips url, adds two event listeners and reformats fetchVideos call + const img select

--- script.js
+++ script.js
@@ -6,11 +6,14 @@ Outline of functions
 4. DONE Connect html id = "randomizer" button to randomizer function
 5. Create another "like" button that saves a video to a sidebar with an id of "saved-videos"
 6. After clicking a video on the side bar, populate main info + video element with video selected
-7. Connect movie select dropdown to allow for users to adjust the video returned from randomizer button to a specific movie with the default behavior being all movies
+7. DONE Connect movie select dropdown to allow for users to adjust the video returned from randomizer button to a specific movie with the default behavior being all movies
 */
 const URL = [
   "http://localhost:3000/clips",
 ];
+const savedVideos = [
+  "http://localhost:3000/savedclips"
+]
 const RANDOMIZE_BTN = document.getElementById("randomize")
 
 const fetchVideos = async (URL, selectedName) => {
@@ -25,7 +28,7 @@ const fetchVideos = async (URL, selectedName) => {
 const videoLoad = (video) => {
   const videoInfo = document.getElementById("video-info")
   const h2 = videoInfo.querySelector("h2")
-  const img = document.getElementById("poster").querySelector("img")
+  const img = document.getElementById("poster")
   const videoPlayer = document.getElementById("video-player")
   h2.textContent = `${video.movie} - ${video.year} - Wow #${video.current_wow_in_movie} of ${video.total_wows_in_movie}`
   img.setAttribute("src", video.poster)
@@ -43,15 +46,11 @@ const videoRandomize = (URL) => {
   return URL.flat()[randomMovieIndex]
 }
 
-const videoLike = () => {
-  console.log("like")
-}
-
 const populateDropdown = (URL) => {
   const select = document.getElementById("movie-select")
   const uniqueNames = {}
   URL.map(movie => {
-    //if name is not unique, execute function
+    //if name is not unique, continue
     if (!uniqueNames[movie["movie"]]){
     const option = document.createElement("option")
     option.setAttribute("value", movie["movie"])
@@ -72,13 +71,16 @@ const handleRandomizeBtn = () => {
 
 
 //call functions
-fetchVideos(URL).then(videos => populateDropdown(videos)).then(videos => videoRandomize(videos)).then(video => videoLoad(video))
+fetchVideos(URL)
+.then(videos => populateDropdown(videos))
+.then(videos => videoRandomize(videos))
+.then(video => videoLoad(video))
+
 //event listeners
 document.getElementById("play").addEventListener("click", videoPlay)
 
 RANDOMIZE_BTN.addEventListener("click", handleRandomizeBtn)
 
-document.getElementById("like").addEventListener("click", videoLike)
-
-
+document.getElementById("poster").addEventListener("mouseenter", (e) => console.log(e))
+document.getElementById("poster").addEventListener("mouseleave", (e) => console.log(e))
 
